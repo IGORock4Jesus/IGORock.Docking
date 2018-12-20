cb9d43ec82e6f9915e7f9de8fff176f0627d1ea3
Igor Kozlov <IGORock.guitar@outlook.com>
2018-12-20 14:13:03 +0600

    Добавьте файлы проекта.

---
+++ IGORock.Docking/Container.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace IGORock.Docking
+{
+	interface IContainer
+	{
+		void AddRight(Page page);
+	}
+}
---
+++ IGORock.Docking/Containers/Window.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace IGORock.Docking.Containers
+{
+	/// <summary>
+	/// Логика взаимодействия для FloatingWindowContainer.xaml
+	/// </summary>
+	public partial class FloatingWindowContainer : Window, IContainer
+	{
+		public FloatingWindowContainer()
+		{
+			InitializeComponent();
+		}
+
+		public void AddRight(Page page)
+		{
+			throw new NotImplementedException();
+		}
+
+		private void Window_DragOver(object sender, DragEventArgs e)
+		{
+			if(e.GetPosition(grid).X > this.ActualWidth / 2)
+			{
+				bool found = false;
+				foreach (var child in grid.Children)
+				{
+					if(child is Tools.RightTool)
+					{
+						found = true;
+						break;
+					}
+				}
+				if (!found)
+					grid.Children.Add(new Tools.RightTool());
+			}
+		}
+	}
+}
